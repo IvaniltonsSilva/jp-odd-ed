da0b7dcdce3932978aa5db0fce625b3c9aff6f27
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-28 11:46:25 -0300

    chore: update shareCode to BQ7H3A7GE5A

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=8ONE7RBC812&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=BQ7H3A7GE5A&afp=oa' });
     res.end();
   }
